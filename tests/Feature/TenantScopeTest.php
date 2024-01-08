abe53a975ca36c32f23af694f04f73a6a690547d
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-08 23:41:35 +0300

    Test: model has tenant id

---
+++ tests/Feature/TenantScopeTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\File;
+use Tests\TestCase;
+
+class TenantScopeTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function test_a_model_has_tenant_id_on_migration(): void
+    {
+        $now = now();
+        $this->artisan('make:model Test -m');
+
+        // find the migration file and check it has a tenant_id on it
+        $filename = $now->year . '_' . $now->format('m') . '_' . $now->format('d') . '_' . $now->format('H')
+            . $now->format('i') . $now->format('s') .
+            '_create_tests_table.php';
+
+
+        $this->assertTrue(File::exists(app_path('Models/Test.php')));
+        $this->assertTrue(File::exists(database_path('migrations/'.$filename)));
+        $this->assertStringContainsString('$table->unsignedBigInteger(\'tenant_id\');', File::get(database_path('migrations/'.$filename)));
+
+        // clean up
+        File::delete(database_path('migrations/'.$filename));
+        File::delete(app_path('Models/Test.php'));
+    }
+}
